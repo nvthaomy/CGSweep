9bed70b4ec80414571599a04a58bb0cdf1521d03
My Nguyen <my@ucsb.edu>
2019-09-18 22:15:39 -0700

    change NonbondEneSlope to NonbondEneSlopeInit

--- cgmodel_sweep_EE.py
+++ cgmodel_sweep_EE.py
@@ -126,7 +126,7 @@ def CreateForceField(Sys, Cut, UseLocalDensity, CoordMin, CoordMax, LDKnots, Run
     if RunSpline:
         PSpline = sim.potential.PairSpline(Sys, Filter = sim.atomselect.Pairs, Cut = Cut,
                                            NKnot = NSplineKnots, Label = 'Spline', 
-                                           NonbondEneSlope = "0.25kTperA", BondEneSlope = "0.25kTperA")
+                                           NonbondEneSlopeInit = "0.25kTperA", BondEneSlope = "0.25kTperA")
         if FitSpline:
             Max = 4.
             decay = 0.1
