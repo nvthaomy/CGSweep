17cd7ade3b5e5c4471bff15564a29543c6143d49
My Nguyen <my@ucsb.edu>
2019-09-29 17:33:36 -0700

    edit default params

--- CGSweep.py
+++ CGSweep.py
@@ -8,13 +8,13 @@ import os, sys
 from shutil import copyfile
 #=================
 ''' USER-INPUT '''
-TrajFileDir = 'Trajectories'
+TrajFileDir = 'traj'
 CGModelScript = 'cgmodel_sweep_EE.py'
 SubmitScriptName  = 'submit.sh'
 runwithtestscript = False
-SpecialName     = 'VarN_Option2'
+SpecialName     = 'NoP'
 NumberThreads = 1
-JobRunTime = '140:00:00'
+JobRunTime = '500:00:00'
 
 #----------------------
 #System related options
@@ -52,16 +52,16 @@ ScaleRuns 			= True
 RunStepScaleList 	= [[3,1],[3,1],[4,1],[4,1],[5,2],[5,2],[10,4],[10,4]] # scales the CG runtime for systems in the NMolList, i.e. run dilute system longer, same size as NMolList (list of list if doing expanded ensemble)
 SysLoadFF 			= True # to seed a run with an already converged force-field. if True, need to specify ff file below, ff file must be in TrajFileDir 
 force_field_file 	= 'CG_run_OptSpline_converged_ff.dat' 
-StepsEquil 		  	= 100000
-StepsProd 			= 1000000
-StepsStride 		= 10
+StepsEquil 		  	= 5e5
+StepsProd 			= 5e6
+StepsStride 		= 100
 
 #--------------------------
 #Options for pair potential
 #--------------------------
-Cut = 30.
+Cut = 20.
 RunSpline = True
-NSplineKnots = 7
+NSplineKnots = 15
 SplineOption = "'Option2'" # Turns on Constant slope for first opt.; then shuts it off for final opt.
 SplineConstSlope = True # NOT USED ANYMORE, Superseeded by SplineOption
 FitSpline = False # Turns on Gaussian Fit of the spline for the initial guess
@@ -79,8 +79,8 @@ PlaneLoc = 0.
 #Options for MD on converged CG model (MD steps are scaled by RunStepScaleList)
 #------------------------------------------------------------------------------
 NSteps_Min = 1000
-NSteps_Equil = 10e6
-NSteps_Prod = 25e6
+NSteps_Equil = 2e6
+NSteps_Prod = 50e6
 WriteFreq = 5000
 
 # parameter names and their values; need to specify trajectorylist above 
